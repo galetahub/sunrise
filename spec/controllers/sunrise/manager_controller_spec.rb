f7379c7a1df0a7a53dfd795e6b26e0ca82facbcf
Igor Galeta <galeta.igor@gmail.com>
2012-01-05 15:51:16 +0200

    Controller spec tests

--- spec/controllers/sunrise/manager_controller_spec.rb
+++ spec/controllers/sunrise/manager_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Sunrise::ManagerController do
+  #render_views
+  
   describe "#current_ability" do
     before(:each) { @ability = controller.send(:current_ability) }
     
@@ -10,6 +12,32 @@ describe Sunrise::ManagerController do
     
     it "should be a guest user" do
       @ability.user.should be_new_record
+    end
+  end
+  
+  describe "admin" do
+    login_admin
+    
+    context "index" do
+      before(:all) do
+        @root = FactoryGirl.create(:structure_main)
+        @page = FactoryGirl.create(:structure_page, :parent => @root)
+      end
+      
+      it "should respond successfully" do
+        get :index, :model_name => "structures"
+        
+        assigns(:records).should include(@root)
+        assigns(:records).should_not include(@page)
+        
+        response.should render_template('index')
+      end
+      
+      it "should render 404 page" do
+        lambda {
+          get :index, :model_name => "wrong"
+        }.should raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end
