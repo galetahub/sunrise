500f534e0c3a95da1004d3b8c4b3c013c19fb3ca
superp <superp1987@gmail.com>
2012-01-16 10:55:58 +0200

    add sunrise abstract models to genetator

---
+++ lib/generators/sunrise/templates/models/sunrise/sunrise_structure.rb
@@ -0,0 +1,22 @@
+class SunriseStructure < Sunrise::AbstractModel
+  self.resource_name = "Structure"
+  
+  default_list_view :tree
+  
+  list :tree do    
+    field :title
+    field :updated_at
+    field :id
+  end
+  
+  edit do
+    field :title
+    field :redirect_url
+    field :slug
+    #field :headers, :partial => true
+    field :parent_id, :collection => [], :if => lambda { |s| s.moveable? }
+    field :kind, :collection => StructureType.all, :include_blank => false
+    field :position, :collection => PositionType.all, :include_blank => false
+    field :is_visible, :boolean => true
+  end
+end
