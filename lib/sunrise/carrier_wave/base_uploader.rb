c09bba28058e8b330cf861351332233741f9c20c
Igor Galeta <galeta.igor@gmail.com>
2012-01-16 10:54:12 +0200

    Fix mini_magick-3.4 issue: can't convert Fixnum into String

--- lib/sunrise/carrier_wave/base_uploader.rb
+++ lib/sunrise/carrier_wave/base_uploader.rb
@@ -30,7 +30,7 @@ module Sunrise
       # process :quality => 85
       def quality(percentage)
         manipulate! do |img|
-          img.quality(percentage)
+          img.quality(percentage.to_s)
           img = yield(img) if block_given?
           img
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # Configure Rails Envinronment
 ENV["RAILS_ENV"] = "test"
 
+require 'fileutils'
+FileUtils.rm_rf(File.expand_path("../tmp/", __FILE__))
+
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 require "rspec/rails"
