dc6f48d03c24f06df2c69f66d68eb202158abbd3
Igor Galeta <galeta.igor@gmail.com>
2013-01-14 18:00:41 +0200

    Dynamic navigation menu

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -2,6 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+unless defined?(SUNRISE_ORM)
+  SUNRISE_ORM = (ENV["SUNRISE_ORM"] || :active_record).to_sym
+end
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
